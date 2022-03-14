6131e26630123493cd727e948090c89149f389bb
btramel <bradtramel@gmail.com>
2022-03-14 09:32:48 -0500

    styles nearly complete. material icons added for addItem and menu buttons, color scheme clean

---
+++ main.js
@@ -0,0 +1,13 @@
+// additembutton
+    // addtitle
+    // addpriority
+    // Details: ...
+// deleteitembutton
+// searchbar
+    // foreach filter through title, details
+// editdetailsbutton
+    // ontouchitem
+    // contenteditable details section
+    // exit-edit-screen
+// sort by priority
+
