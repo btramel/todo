27861a7e8e7ce1388e2660a31ac3df5e91a249aa
btramel <bradtramel@gmail.com>
2022-03-17 11:37:09 -0500

    delete, expand, strikethrough, priority all functional. need to add styling for expandable elements, implement a searchfeature, and ease into expansion

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 class Item {
-    constructor(title, priority, details) {
+    constructor(title, priority, details, tag) {
         this.title = title;
         this.priority = priority;
         this.details = details;
+        this.tag = tag;
     }
 }
 
-// initialize array with to-do list items (objects)
-const items = [{title: 'Feed Mox', priority: null, details: 'treats too?'}]
+const LOCAL_STORAGE_ITEMS_KEY = 'items.key'
+const LOCAL_STORAGE_NAME_KEY = 'name.key'
+let username = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_KEY)) || undefined
 
-
+// initialize array with to-do list items (objects) + local storage
+let items = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEMS_KEY)) || []
+let caseyArr = [
+    {title: 'Drink water!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Stretch!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Read a little poetry!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Yoga!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: '2 minutes deep breathing!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Indulge in a sweet treat!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Fuck it -- send a poem to the New Yorker!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Think of everyone who loves you!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Take stock of your accomplishments!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Another butt tattoo?', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Call Brad', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+    {title: 'Take a long, hot bath!', priority: true, details: 'A reminder, with love, from Honeybee 🐝'},
+];
 
 // SELECTORS
 const menu = document.querySelector('.menu')
@@ -18,11 +35,10 @@ const modal = document.querySelector('.modal')
 const newItem = document.querySelector('#newItem')
 const closeModalButtons = document.querySelectorAll('.close-button')
 const submit = document.getElementById('submit-item')
-
-
+const deleteItem = document.querySelectorAll('.delete')
+const form = document.querySelector('form')
 
 // EVENT LISTENERS
-
 // listen for new item click
 newItem.addEventListener('click', () => {
     openModal()
@@ -30,7 +46,8 @@ newItem.addEventListener('click', () => {
 
 // listen for all closing abilities: close button, submit button, and overlay itself
 closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
         const modal = button.closest('.modal')
         closeModal(modal)
     })
@@ -48,17 +65,36 @@ overlay.addEventListener('click', () => {
 submit.addEventListener('click', (e) => {
     e.preventDefault()
     createItemObj()
-    // sortItems()
-    displayItems()
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    function casey() {
+        if ( username.toLowerCase() === 'casey' && items.length % 4 === 0 ) {
+        items.push( caseyArr[getRandomInt(caseyArr.length - 1)])
+        } else return
+    }
+    casey()
+    saveAndRender()
     modal.classList.remove('active')
     overlay.classList.remove('active')
+    form.reset()
 })
 
-
-
-
-
-// OPEN AND CLOSE
+// SAVE TO LOCAL STORAGE -- HALLELUJAH
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_ITEMS_KEY, JSON.stringify(items))
+    localStorage.setItem(LOCAL_STORAGE_NAME_KEY, JSON.stringify(username))
+}
+// use this primarily. render() to load initial page
+function saveAndRender() {
+    save()
+    render()
+}
+function clearList(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
 function openModal() {
     modal.classList.add('active')
     overlay.classList.add('active')
@@ -66,60 +102,121 @@ function openModal() {
 function closeModal() {
     modal.classList.remove('active')
     overlay.classList.remove('active') }
-
-
 function createItemObj() {
     // initialize
     const title = document.getElementById('title').value
-    let priority;
-        // check to see if the new item is a priority
-        if ( document.getElementById('prioritize').checked ? priority = 1 : priority = null );
-    const details = document.getElementById('details').value
     
-    const item = new Item(title, priority, details);
+    // check to see if the new item is a priority
+    const priority = document.getElementById('prioritize').checked
 
-    // add new item to object array
+    const details = document.getElementById('details').value
+
+    const tag = items.length
+    
+    const item = new Item(title, priority, details, tag)
+
+    // add new item to array
     items.push(item)
     console.table(items)
+}
+function render() {
+    // clear display
+    const container = document.getElementById('list-container')
+    clearList(container)
+
+    // loop through array of object, rendering each
+    items.forEach(item => {
+
+        // initialize node elements
+        const node = document.createElement('div')
+        const titleNode = document.createElement('div')
+        const detailsNode = document.createElement('div')
+        const priorityNode = document.createElement('div')
+        const checkboxNode = document.createElement('input')
+        const deleteNode = document.createElement('button')
+
+
+        // assign class names to nodes
+        node.classList.add('item')
+        node.dataset.itemTag = item.tag
+        titleNode.classList.add('title')
+        detailsNode.classList.add('details')
+        detailsNode.classList.add('hidden')
+        priorityNode.classList.add('priority')
+        deleteNode.classList.add('delete')
+        checkboxNode.type = 'checkbox'
+        checkboxNode.name = 'finished'
+        checkboxNode.id = 'finished'
+        // strikethrough text if checked, and remove strike if unchecked
+        checkboxNode.onclick = function() {
+            let strikeParent = checkboxNode.closest('.item')
+            if ( strikeParent.classList.contains('finished') ) {
+            strikeParent.classList.remove('finished')
+            }   else {
+                strikeParent.classList.add('finished')
+            }
+        }
+
+
+        // distribute values
+        titleNode.innerText = `${item.title}`
+        detailsNode.innerText = `${item.details}`
+        if ( item.priority ? priorityNode.innerText = '!' : priorityNode.innerText = ' ' )
+        deleteNode.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" class="delete" viewBox="0 0 24 24" width="24px" fill="#fffacd"><path class="delete" d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>`
+        
+        node.onclick = function(e) {
+            if ( node.classList.contains('expand') && e.target !== checkboxNode ) { // EVENT DOESN'T FIRE IF CLICK TARGET IS CHECKBOX
+                node.classList.remove('expand')
+                detailsNode.classList.add('hidden')
+            } else if ( e.target !== checkboxNode && detailsNode.innerHTML !== '') { // EVENT DOESN'T FIRE IF NO DETAILS ARE AVAILABLE
+                node.classList.add('expand')
+                detailsNode.classList.remove('hidden')
+            }
+        }
+
+        // clicking on trash icon will remove object and re-render items list
+        deleteNode.onclick = function() {
+                    // gets closest parent div of the garbage can icon, then gets its data tag (which corresponds to its location in the Items array)
+                    const goner = Number(this.closest('.item').getAttribute('data-item-tag'))
+                    // removes that item from the array
+                    items.splice(goner, 1)
+                    // loops through the array and updates the tag attribute of each object, using new position in array
+                    items.forEach(item => {
+                        item.tag = items.indexOf(item)
+                    })
+                    // re-renders page
+                    saveAndRender()
+                    // table log for proof
+                    console.table(items)
+                }
+        
+        // display new card
+        container.append(node)
+        node.appendChild(titleNode)
+        node.appendChild(priorityNode)
+        node.appendChild(checkboxNode)
+        node.appendChild(deleteNode)
+        node.appendChild(detailsNode)
+    })
+    save()
 }
 
 // function sortItems() {
 //     // sort obj array by priority
 // }
 
-function displayItems() {
-    // assign information to card nodes
-    const listContainer = document.querySelector('.list-container')
-    let node = document.createElement('div')
-    node.className = 'item'
-
-    const title = document.getElementById('title').value
-    let titleNode = document.createElement('div')
-    titleNode.classList.add('title')
-    titleNode.innerHTML = `${title}`
-
-    let priorityNode = document.createElement('div');
-    if ( document.getElementById('prioritize').checked ? priority = 1 : priority = null );
-    if ( priority === 1 ) {
-        priorityNode.innerHTML = '!'
-    } else ( priorityNode.innerHTML = ' ');
-    priorityNode.classList.add('priority')
-
-    let deleteNode = document.createElement('button')
-    deleteNode.classList.add('delete')
-    deleteNode.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fffacd"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>`
-
-    // display new card
-    listContainer.append(node)
-    node.appendChild(titleNode)
-    node.appendChild(priorityNode)
-    // node.appendChild(priorityNode)
-    node.appendChild(deleteNode)
-}
-
+// menu functionality?
 // searchbar
     // foreach filter through title, details
+
 // editdetailsbutton
     // ontouchitem
     // contenteditable details section
     // exit-edit-screen
+
+if ( username === undefined || username === null ) {
+    username = prompt('Welcome! What is your name?').toLowerCase()
+    alert(`Cool. Let's do stuff, ${username}`) 
+}
+
+render()
